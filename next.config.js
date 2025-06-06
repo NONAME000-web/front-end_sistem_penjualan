e4046a92be0e78ac79fd911fd4fa5d6da53b02b2
NONAME000-web <adeferdi19@gmail.com>
2025-06-06 20:56:55 +0800

    update-2

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Tambahkan ini jika Anda ingin static export
+  // output: 'export', 
+}
+
+module.exports = nextConfig
